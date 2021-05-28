01e4f7d140b7d50496226d62184d2c8fb213aa9c
dariuszsporna <47335340+dariuszsporna@users.noreply.github.com>
2021-05-28 16:03:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const pass = document.querySelector('#password');
+const p = document.querySelector('.passinfo');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
+
+const chceckPassword = () => {
+    if (pass.value.lenght > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)){
+        p.innerHTML = 'Masz bardzo dobre hasło!';
+        p.style.color = 'lime';
+    } else if (pass.value.lenght > minValue && pass.value.match(letters) && pass.value.match(numbers)){
+        p.innerHTML = ' Masz dobre hasło.';
+        p.style.color = 'gold';
+    } else {
+        p.innerHTML = ' Masz za słabe hasło';
+        p.style.color = 'tomato';
+    }
+};
+
+pass.addEventListener('keyup', function(){
+    if(pass.value !== ''){
+        chceckPassword();
+    } else {
+        p.innerHTML = 'Nie podałeś hasła...';
+        p.style.color = '';
+    };
+});
